23979a20115869d8713eab3bad4a0b571773f439
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-09-25 01:03:29 +0330

    debugging ...

--- model/contactBotoStart.js
+++ model/contactBotoStart.js
@@ -5,7 +5,7 @@ const contactBotoSchema = new Schema(
     firstName: { type: String, required: true },
     lastName: { type: String, default: "" },
     email: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
   },
   { timestamps: true }
 );
